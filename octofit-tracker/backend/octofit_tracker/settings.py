6ed5d56cf384931a4a0e253bf831b1658c8c1cf7
Gleica Pires Reinert <gleicareinert@gmail.com>
2025-04-25 16:05:45 +0000

    Etapa 4

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'sturdy-trout-4q9xpppxr6q257pr-8000.app.github.dev']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
