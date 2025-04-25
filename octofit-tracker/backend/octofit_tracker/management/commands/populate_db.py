9e14c21d939875f09d4a1c0ccf0b9a5c04e2eaed
Gleica Pires Reinert <gleicareinert@gmail.com>
2025-04-25 15:38:30 +0000

    Correção 5 etapa 3

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,7 +1,7 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand # type: ignore
 from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
 from datetime import timedelta
-from bson import ObjectId
+from bson import ObjectId # type: ignore
 
 class Command(BaseCommand):
     help = 'Popula o banco de dados octofit_db com dados de teste.'
